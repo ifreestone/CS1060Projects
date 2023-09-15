298e2db92fb3498d436392f74c4278371b5fd834
ifreestone <143460005+ifreestone@users.noreply.github.com>
2023-09-15 17:34:46 -0600

    Finished CircleCalculation assignment

--- src/CircleCalculation.java
+++ src/CircleCalculation.java
@@ -1,19 +1,38 @@
 import java.util.Scanner;
-//public class CircleCalculation {
-    //public static void main(String[] args) {
-        //int radius = 2;
-        //int height = 4;
-        //area(radius);
-        //System.out.println(area);
-        //circumference(radius);
-        //System.out.println(volume);
-    //}
+public class CircleCalculation {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //Asks the user for the radius and then puts it into the double named "radius"
+        System.out.println("Please input the circles radius:");
+        double radius = sc.nextDouble();
 
-    //public static double area(int radius, double areaCircle){
-        //return areaCircle = Math.PI * (radius * radius);
-    //}
-    //public static double circumference(int radius, double circumferenceCircle){
-        //return circumferenceCircle = 2 * Math.PI * radius;
-    //}
-//}
-//}
+        //If statement that makes sure the radius entered is valid (not negative)
+        if (radius >= 0){
+            //There take the answer from a method and set it to a double for printing out
+            double diameterResult = diameter(radius);
+            System.out.println("The diameter is:" + diameterResult);
+            double areaResult = area(radius);
+            System.out.println("The area is: " + areaResult);
+            double cirResult = circumference(radius);
+            System.out.println("The circumference is: " + cirResult);
+        } else {
+            //Prints out error in-case the user inputs a non-valid radius
+            System.out.println("Error");
+        }
+    }
+    //Method that calculates the diameter from the user inputted radius before returning the result
+    public static double diameter(double radius){
+        double diameterCircle = radius + radius;
+        return diameterCircle;
+    }
+    //Method that calculates the area from the user inputted radius before returning the result
+    public static double area(double radius){
+        double areaCircle = Math.PI * (radius * radius);
+        return areaCircle;
+    }
+    //Method that calculates the circumference from the user inputted radius before returning the result
+    public static double circumference(double radius){
+        double cirCircle = 2 * Math.PI * radius;
+        return cirCircle;
+    }
+}
